c4312b04fb9ed88a2ced5965c0518576a50adfda
Alf Mikael <amikaelc@protonmail.com>
2022-12-07 16:18:49 +0100

    commit

--- Prosjekt/SolveModel_main.py
+++ Prosjekt/SolveModel_main.py
@@ -4,6 +4,7 @@ import SolverAlgorithms as algs
 num_nodes = 9
 model = models.SimplySupportedBeamModel(num_nodes)
 #model = models.CantileverWithEndMoment(num_nodes)
+# model 3
 
 algs.solveLinearSteps(model,load_steps=0.01,max_steps=100)
 #algs.solveNonlinLoadControl(model,load_steps=0.01,max_steps=100)
