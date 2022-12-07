509e56052edc06bbd87c18709743351a9a8a4c9d
Alf Mikael <amikaelc@protonmail.com>
2022-12-07 17:31:27 +0100

    commit

--- Prosjekt/SolveModel_main.py
+++ Prosjekt/SolveModel_main.py
@@ -6,9 +6,9 @@ model = models.SimplySupportedBeamModel(num_nodes)
 #model = models.CantileverWithEndMoment(num_nodes)
 # model 3
 
-algs.solveLinearSteps(model,load_steps=0.01,max_steps=100)
+#algs.solveLinearSteps(model,load_steps=0.01,max_steps=100)
 #algs.solveNonlinLoadControl(model,load_steps=0.01,max_steps=100)
-#algs.solveArchLength(model,load_steps=0.01,max_steps=100)
+algs.solveArchLength(model,archLength=0.01,max_steps=100)
 
 num_steps = len(model.load_history)
 
